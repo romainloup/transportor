5784cf4f82c516e4bc54e20673070b3bac16102d
romainloup <38253940+romainloup@users.noreply.github.com>
2019-09-04 09:47:31 +0200

    Update setRouterOTP.R

--- transportorPreject/setRouterOTP.R
+++ transportorPreject/setRouterOTP.R
@@ -8,5 +8,6 @@ library(opentripplanner)
 # Set the server
 otp = paste0(getwd(), "/otp-1.4.0-shaded.jar")
 graphdir <- paste0(getwd(), "/graphs/default")
-# Allocate the RAM (in Mo) and set the name of the router, possible to allocate less memory
+# Allocate the RAM (in Mo) and set the name of the router, possible to allocate less memory but it will be very slow
+# Be sure to have Java insalled
 otp_setup(otp, getwd(), memory = 8000, router = "default") # possible to change the port adding 'port = 8081' before the last bracket
